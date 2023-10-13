bcf8b9320df9e180613838684114c21bde9ec44d
Luther Power <post2base@outlook.com>
2023-10-13 20:57:39 +0100

    Slight refactor for naming consistency.

--- terraform/rancher/external_hrv.tf
+++ terraform/rancher/external_hrv.tf
@@ -1,6 +1,6 @@
-resource "rancher2_cloud_credential" "mission-ctrl-harvester-external" {
-  name = "mission-ctrl-harvester-external"
-  description= "cloudCredentials for 'mission-ctrl' Harvester cluster"
+resource "rancher2_cloud_credential" "mission-ctrl-harvester-external-creds" {
+  name = "mission-ctrl-harvester-external-creds"
+  description = "cloudCredentials for 'mission-ctrl' Harvester cluster"
   harvester_credential_config {
     cluster_type = "external"
     kubeconfig_content = file("$HOME/.kube/mission-ctrl-harvester-kubeconfig.yaml")
--- terraform/rancher/imported_hrv.tf
+++ terraform/rancher/imported_hrv.tf
@@ -1,8 +1,8 @@
 data "rancher2_cluster_v2" "mission-ctrl-harvester-imported" {
   name = "mission-ctrl-harvester-imported"
 }
-resource "rancher2_cloud_credential" "mission-ctrl" {
-  name = "mission-ctrl-harvester-imported"
+resource "rancher2_cloud_credential" "mission-ctrl-harvester-imported-creds" {
+  name = "mission-ctrl-harvester-imported-creds"
   harvester_credential_config {
     cluster_id = data.rancher2_cluster_v2.mission-ctrl-harvester-imported.cluster_v1_id
     cluster_type = "imported"
