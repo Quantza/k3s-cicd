3ed79726577a392e7f52356c71354aee34ac0bd4
Luther Power <post2base@outlook.com>
2023-10-21 20:56:07 +0100

    Update formatting for cloud configs; ssh key only under provided user.

--- terraform/harvester/cloud-config.tf
+++ terraform/harvester/cloud-config.tf
@@ -7,38 +7,38 @@ resource "harvester_cloudinit_secret" "cloud-config-docker-ubuntu22-04" {
   ]
 
   user_data    = <<EOF
-    #cloud-config
-    users:
-      - name: quantza
-        sudo: ALL=(ALL) NOPASSWD:ALL
-        passwd: $6$tCgT7xBA.tJf62Mf$Lo7mC7QynUVu0b3CcQR0GP2ELOaNivVCCcWXwtR44SyDuf/MlpoHXimCMoRy1Dq4m7lpCeQy4Q1vaO//Z6wkx0
-        lock_passwd: false
-        shell: /bin/bash
-
-    packages:
-      - curl
-      - wget
-      - btop
-
-    write_files:
-      - path: /tmp/install_docker.sh
-    content: |
-        #!/bin/bash
-        curl -fsSL https://get.docker.com -o get-docker.sh
-        sh get-docker.sh
-        permissions: '0755'
-
-    runcmd:
-      - sudo apt update && sudo apt upgrade -y
-      - sudo /tmp/install_docker.sh
-      - - systemctl
-        - enable
-        - '--now'
-        - qemu-guest-agent
+#cloud-config
+users:
+  - name: quantza
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    passwd: $6$tCgT7xBA.tJf62Mf$Lo7mC7QynUVu0b3CcQR0GP2ELOaNivVCCcWXwtR44SyDuf/MlpoHXimCMoRy1Dq4m7lpCeQy4Q1vaO//Z6wkx0
     ssh_authorized_keys:
       - >-
         public_key content of harvester_ssh_key.vm-sshkey
-    EOF
+    lock_passwd: false
+    shell: /bin/bash
+
+packages:
+  - curl
+  - wget
+  - btop
+
+write_files:
+  - path: /tmp/install_docker.sh
+content: |
+    #!/bin/bash
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    permissions: '0755'
+
+runcmd:
+  - sudo apt update && sudo apt upgrade -y
+  - sudo /tmp/install_docker.sh
+  - - systemctl
+    - enable
+    - '--now'
+    - qemu-guest-agent
+EOF
   network_data = ""
 }
 
@@ -51,27 +51,27 @@ resource "harvester_cloudinit_secret" "cloud-config-main-ubuntu22-04" {
   ]
 
   user_data    = <<EOF
-    #cloud-config
-    users:
-      - name: quantza
-        sudo: ALL=(ALL) NOPASSWD:ALL
-        passwd: $6$tCgT7xBA.tJf62Mf$Lo7mC7QynUVu0b3CcQR0GP2ELOaNivVCCcWXwtR44SyDuf/MlpoHXimCMoRy1Dq4m7lpCeQy4Q1vaO//Z6wkx0
-        lock_passwd: false
-        shell: /bin/bash
-    package_update: true
-    packages:
-      - qemu-guest-agent
-      - curl
-      - wget
-      - btop
-    runcmd:
-      - - systemctl
-        - enable
-        - '--now'
-        - qemu-guest-agent.service
+#cloud-config
+users:
+  - name: quantza
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    passwd: $6$tCgT7xBA.tJf62Mf$Lo7mC7QynUVu0b3CcQR0GP2ELOaNivVCCcWXwtR44SyDuf/MlpoHXimCMoRy1Dq4m7lpCeQy4Q1vaO//Z6wkx0
     ssh_authorized_keys:
       - >-
         public_key content of harvester_ssh_key.vm-sshkey
-    EOF
+    lock_passwd: false
+    shell: /bin/bash
+package_update: true
+packages:
+  - qemu-guest-agent
+  - curl
+  - wget
+  - btop
+runcmd:
+  - - systemctl
+    - enable
+    - '--now'
+    - qemu-guest-agent.service
+EOF
   network_data = ""
 }
