fda196385e259faff0dd1d724a5bccde0ac18495
Luther Power <post2base@outlook.com>
2023-10-21 21:30:32 +0100

    Update formatting for VMs config

--- terraform/rancher/k3s-vms.tf
+++ terraform/rancher/k3s-vms.tf
@@ -5,22 +5,22 @@ resource "rancher2_machine_config_v2" "mission-ctrl-hrv-etcd-v2" {
     cpu_count = "2"
     memory_size = "4"
     disk_info = <<EOF
-    {
-        "disks": [{
-            "name": "rootdisk",
-            "imageName": "${var.vm_os-image.namespace}/${var.vm_os-image.id}",
-            "size": 32,
-            "bootOrder": 1
-        }]
-    }
-    EOF
+{
+    "disks": [{
+        "name": "rootdisk",
+        "imageName": "${var.vm_os-image.namespace}/${var.vm_os-image.id}",
+        "size": 32,
+        "bootOrder": 1
+    }]
+}
+EOF
     network_info = <<EOF
-    {
-        "interfaces": [{
-            "networkName": "${var.main-vlan.namespace}/${var.main-vlan.name}"
-        }]
-    }
-    EOF
+{
+    "interfaces": [{
+        "networkName": "${var.main-vlan.namespace}/${var.main-vlan.name}"
+    }]
+}
+EOF
     ssh_user = var.vm_user.name
     user_data = var.cloud-config-main-ubuntu22-04.user_data
   }
@@ -33,22 +33,22 @@ resource "rancher2_machine_config_v2" "mission-ctrl-hrv-cplane-v2" {
     cpu_count = "2"
     memory_size = "6"
     disk_info = <<EOF
-    {
-        "disks": [{
-            "name": "rootdisk",
-            "imageName": "${var.vm_os-image.namespace}/${var.vm_os-image.id}",
-            "size": 20,
-            "bootOrder": 1
-        }]
-    }
-    EOF
+{
+    "disks": [{
+        "name": "rootdisk",
+        "imageName": "${var.vm_os-image.namespace}/${var.vm_os-image.id}",
+        "size": 20,
+        "bootOrder": 1
+    }]
+}
+EOF
     network_info = <<EOF
-    {
-        "interfaces": [{
-            "networkName": "${var.main-vlan.namespace}/${var.main-vlan.name}"
-        }]
-    }
-    EOF
+{
+    "interfaces": [{
+        "networkName": "${var.main-vlan.namespace}/${var.main-vlan.name}"
+    }]
+}
+EOF
     ssh_user = var.vm_user.name
     user_data = var.cloud-config-main-ubuntu22-04.user_data
   }
@@ -61,28 +61,28 @@ resource "rancher2_machine_config_v2" "mission-ctrl-hrv-wrkr-v2" {
     cpu_count = "2"
     memory_size = "6"
     disk_info = <<EOF
+{
+    "disks": [{
+        "name": "rootdisk",
+        "imageName": "${var.vm_os-image.namespace}/${var.vm_os-image.id}",
+        "size": 64,
+        "bootOrder": 1
+    },
     {
-        "disks": [{
-            "name": "rootdisk",
-            "imageName": "${var.vm_os-image.namespace}/${var.vm_os-image.id}",
-            "size": 64,
-            "bootOrder": 1
-        },
-        {
-            "name": "extradisk",
-            "storageClassName": "harvester-longhorn",
-            "size": 16,
-            "bootOrder": 2
-        }]
-    }
-    EOF
+        "name": "extradisk",
+        "storageClassName": "harvester-longhorn",
+        "size": 16,
+        "bootOrder": 2
+    }]
+}
+EOF
     network_info = <<EOF
-    {
-        "interfaces": [{
-            "networkName": "${var.main-vlan.namespace}/${var.main-vlan.name}"
-        }]
-    }
-    EOF
+{
+    "interfaces": [{
+        "networkName": "${var.main-vlan.namespace}/${var.main-vlan.name}"
+    }]
+}
+EOF
     ssh_user = var.vm_user.name
     user_data = var.cloud-config-main-ubuntu22-04.user_data
   }
